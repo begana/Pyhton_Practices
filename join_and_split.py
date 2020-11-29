3f3326e638131089b2bfa0f4c571cffc766bc4a8
Begana Choi <begana001.jpg@gmail.com>
2020-11-29 17:05:47 +0100

    Practice join and split

---
+++ join_and_split.py
@@ -0,0 +1,13 @@
+attendees = ['Ken', 'Alena', 'Treasure']
+optional_attendees = ['Begana', 'Isu']
+
+to_line = ", ".join(attendees)
+cc_line = ", ".join(optional_attendees)
+print("to : " + to_line)
+print("cc : " + cc_line)
+
+words = "The greatest teacher failure is"
+print(words.split())
+
+words2 = ['The', 'greatest', 'teacher', 'failure', 'is']
+print(" ".join(words2))
