b7a37f5af629fae5ef97d435324bcc77212009c3
Begana Choi <begana001.jpg@gmail.com>
2020-11-29 15:49:30 +0100

    Practice comaprison

--- python_basics.py
+++ python_basics.py
@@ -1,6 +1,12 @@
-first_name = input("what is your name? ")
-print("hello,", first_name)
-print(first_name, "is learning Python.")
 
-current_mood = input("how do you feel today? ")
-print("{}, don't feel too {}!".format(first_name, current_mood))
+first_name = input("What is your name? ")
+print("hello,", first_name)
+if first_name == 'Begana':
+    print(first_name, "is learning Python.")
+else:
+    age = int(input("how old are you? "))
+    if age <= 6:
+        print("wow, you are {}! if you are confident with your reading already, you should learn Python.".format(age))
+    else:
+        print("you should totally learn Python, {}!".format(first_name))
+    
