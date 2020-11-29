5571b16b40f211e289989f68c0da607217269294
Begana Choi <begana001.jpg@gmail.com>
2020-11-29 15:43:00 +0100

    Practice init, get, new

---
+++ python_basics.py
@@ -0,0 +1,6 @@
+first_name = input("what is your name? ")
+print("hello,", first_name)
+print(first_name, "is learning Python.")
+
+current_mood = input("how do you feel today? ")
+print("{}, don't feel too {}!".format(first_name, current_mood))
