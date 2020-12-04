8885b0a45b83dbfcb562a0c727eff79c94ccebee
Begana Choi <begana001.jpg@gmail.com>
2020-12-04 13:17:34 +0100

    Build YatzyHands

--- dice/dice.py
+++ dice/dice.py
@@ -35,6 +35,9 @@ class Die:
     def __radd__(self, other):
         return int(self) + other
     
+    def __repr__(self):
+        return str(self.value)
+    
 class D6(Die):
     def __init__(self):
         super().__init__(sides=6, value=0)
